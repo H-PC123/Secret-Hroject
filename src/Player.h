3c3e96894dc8f989f76a08632d2f630baed58ad7
H-PC123 <chen.paulb@gmail.com>
2018-10-13 01:03:11 -0400

    adding the source directory subject to change in the future

---
+++ src/Player.h
@@ -0,0 +1,16 @@
+/*
+ * Player.h
+ *
+ *  Created on: Oct. 12, 2018
+ *      Author: hpc123
+ */
+
+#ifndef SRC_PLAYER_H_
+#define SRC_PLAYER_H_
+
+class player{
+	private string name;
+	private int playerNo;
+};
+
+#endif /* SRC_PLAYER_H_ */
